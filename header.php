b3a27d1689ecd242580a25a71ddaad70f87392db
Tuyen Phan <tuyenphan@advn.vn>
2025-11-03 22:45:59 +0700

    a

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
           'depth' => 2,
         )); ?>
       </div>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
    
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
